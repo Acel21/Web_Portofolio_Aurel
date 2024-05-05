f0ad1184cf64dcff01f076dcfb0a1cb5c2534aae
Acel21 <Aurelia.sbrn21@gmail.com>
2024-05-05 13:19:24 +0700

    Web Portofolio Aurel

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrolltoabout () {
+    const aboutsection = document.getElementById ("about")
+    aboutsection && aboutsection.scrollIntoView()
+}
+
+function scrolltoskill () {
+    const skillsection = document.getElementById ("skills")
+    skillsection && skillsection.scrollIntoView()
+}
+
+function scrolltocontact () {
+    const contactsection = document.getElementById ("Contact")
+    contactsection && contactsection.scrollIntoView()
+}
